d15bf01eadac36e4171acfba3b0928ecd1a33a65
Gabrijel Boduljak <gboduljak@outlook.com>
2021-07-21 00:27:37 +0100

    sorts are coming back

--- test/tests-cases/sorting/insertion-sort/insertion-sort-program.c
+++ test/tests-cases/sorting/insertion-sort/insertion-sort-program.c
@@ -1,15 +1,23 @@
 #include "insertion-sort.h"
+// #include <stdio.h>
+// #include <stdlib.h>
 
 int main() {
-  int length = 0;
+  int length;
+  length = 0;
+
   scanf("%d", &length);
-  int i = 0;
-  int *A = malloc(sizeof(int) * length);
-  for (i = 0; i < length; i=i+1) {
+  int i;
+  i = 0;
+
+  int *A;
+  A = malloc(sizeof(int) * length);
+
+  for (i = 0; i < length; i = i + 1) {
     scanf("%d", &A[i]);
   }
   InsertionSort(A, length);
-  for (i = 0; i < length; i=i+1) {
+  for (i = 0; i < length; i = i + 1) {
     printf("%d ", A[i]);
   }
   free(A);
