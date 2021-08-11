4d354192f7defac9cc46753de0e2525d9384b127
Gabrijel Boduljak <gboduljak@outlook.com>
2021-08-11 02:04:43 +0100

    removed work in progress codegen test folder

--- test/cgen-wip/array.c
+++
@@ -1,7 +0,0 @@
-int main () {
-  int arr[10][20];
-  int x;
-  arr[1][1] = 10; 
-  x = arr[1][1];
-  printf("%d", x);
-}
--- test/cgen-wip/loop.c
+++
@@ -1,7 +0,0 @@
-int main() {
-  int i;
-
-  for(i = 0; i < 10; i=i+1) {
-    printf("%d\n", i);
-  }
-}
